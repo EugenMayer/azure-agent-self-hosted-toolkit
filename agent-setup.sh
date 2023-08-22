229f7d4ba37597fef340ab7991c4558c7064789a
Shahar Hayon <shaharh@nanobebe.com>
2023-08-22 14:53:51 +0300

    remove -v from curl command

--- agent-setup.sh
+++ agent-setup.sh
@@ -46,7 +46,7 @@ export AGENT_INSTALL_DIR=$AGENT_USER_HOME/agent
 
 echo "Downloading agent"
 mkdir -p $AGENT_INSTALL_DIR
-curl -f -v -o $AGENT_INSTALL_DIR/agent.tar.gz https://vstsagentpackage.azureedge.net/agent/$AGENT_VERSION/vsts-agent-linux-$ARCH-$AGENT_VERSION.tar.gz
+curl -f -o $AGENT_INSTALL_DIR/agent.tar.gz https://vstsagentpackage.azureedge.net/agent/$AGENT_VERSION/vsts-agent-linux-$ARCH-$AGENT_VERSION.tar.gz
 cd $AGENT_INSTALL_DIR
 tar -xf agent.tar.gz
 rm -f $AGENT_INSTALL_DIR/agent.tar.gz
